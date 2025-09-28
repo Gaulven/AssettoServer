08ae6687ea625f1064646f9ad6980cf78c469ec2
Gaulven <gaulven@gaulven.com>
2025-09-28 16:14:08 -0700

    Backport multiple loading screens from 0.0.55

--- AssettoServer/Server/Configuration/ACExtraConfiguration.cs
+++ AssettoServer/Server/Configuration/ACExtraConfiguration.cs
@@ -87,8 +87,8 @@ public partial class ACExtraConfiguration : ObservableObject
     public bool EnableLegacyPluginInterface { get; init; } = false;
     [YamlMember(Description = "Automatically configure port forwards using UPnP or NAT-PMP. Empty = Enable on Windows when lobby registration is enabled. true = Always enable, detailed error log. false = Always disable")]
     public bool? EnableUPnP { get; init; } = false;
-    [YamlMember(Description = "URL of custom loading screen image. Requires CSP 0.1.80+ and a recent version of Content Manager")]
-    public string? LoadingImageUrl { get; set; }
+    [YamlMember(Description = "List of URLs for custom loading screen images. A random image will be picked from this list. Requires CSP 0.2.0+ and a recent version of Content Manager")]
+    public List<string>? LoadingImageUrls { get; set; }
     [YamlMember(Description = "Name and path of file-based user groups")]
     public Dictionary<string, string> UserGroups { get; init; } = new()
     {
